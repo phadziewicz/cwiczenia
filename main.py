96995493711ac239a3f1feeb9184eb62f2fcf0a5
phadziewicz <you@example.com>
2021-03-03 18:07:42 +0100

    8 zadań

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#zad1
+# a='kot'
+# b='pies'
+# c=2
+# d=3
+# e=2.4
+# f=5.5
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+#zad2
+# l1=10
+# l2=2
+# suma=l1+l2
+# roznica=l1-l2
+# iloczyn=l1*l2
+# iloraz=l1/l2
+# print(suma)
+# print(roznica)
+# print(iloczyn)
+# print(iloraz)
+#zad3
+# i=3
+# print(i)
+# i+=2
+# print(i)
+# i-=2
+# print(i)
+# i*=2
+# print(i)
+# i/=2
+# print(i)
+# i%=2
+# print(i)
+# i**=2
+# print(i)
+#zad4
+# from math import *
+# eu=e**10
+# print(eu)
+#
+# a=log(5+sin(8)**2)
+# b=pow(a,1/6)
+# print(a)
+#
+# print(floor(3.55))
+#
+# print(ceil(4.80))
+#zad5
+# imie='PIOTR'
+# nazwisko='HADZIEWICZ'
+# print(imie.capitalize())
+# print(nazwisko.capitalize())
+#zad6
+tekst='lala lala la'
+# policz=tekst.count('la')
+# print(policz)
+#zad7
+# napis='Hadziewicz'
+# print(napis[1])
+# print(napis[-1])
+#zad8
+# nowy=tekst.split(' ')
+# print(nowy)
